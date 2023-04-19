bf16f9e2c6a7ca9655e770394778931ccfde28fe
susanna.sjoberg <susanna.sjoberg@ahlens.se>
2023-04-19 16:13:41 +0200

    changed models

--- server.js
+++ server.js
@@ -1,102 +1,153 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose, { Schema } from "mongoose";
 
-import skotrum from './data/skotrum.json';
-import gbgSkotrum from './data/gbgSkotrum.json';
-import malmoSkotrum from './data/malmoSkotrum.json';
+import skotrum from "./data/skotrum.json";
+import gbgSkotrum from "./data/gbgSkotrum.json";
+import malmoSkotrum from "./data/malmoSkotrum.json";
 
 //const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/skotrum-finalP';
-const mongoURI = 'mongodb+srv://sansjo:technigo19@cluster0.xevnw.mongodb.net/skotrum-finalP?retryWrites=true&w=majority'
+const mongoURI =
+	"mongodb+srv://sansjo:technigo19@cluster0.xevnw.mongodb.net/skotrum-finalP?retryWrites=true&w=majority";
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.Promise = Promise;
 
-
-const BabyRoomMalmo = mongoose.model('BabyRoomMalmo', {
-  name: String,
-  address: String,
-  phone: String,
-  openHours: String,
-  note: String,
-  website: String,
-  latitude: Number,
-  longitude: Number
+const babyroommalmo = new Schema({
+	name: String,
+	address: String,
+	phone: String,
+	openHours: String,
+	note: String,
+	website: String,
+	latitude: Number,
+	longitude: Number,
 });
+const BabyRoomMalmo = mongoose.model("BabyRoomMalmo", babyroommalmo);
 
-const BabyRoomGbg = mongoose.model('BabyRoomGbg', {
-  name: String,
-  address: String,
-  phone: String,
-  openHours: String,
-  note: String,
-  website: String,
-  latitude: Number,
-  longitude: Number,
+// const BabyRoomMalmo = mongoose.model("BabyRoomMalmo", {
+// 	name: String,
+// 	address: String,
+// 	phone: String,
+// 	openHours: String,
+// 	note: String,
+// 	website: String,
+// 	latitude: Number,
+// 	longitude: Number,
+// });
+
+const babyroomgbg = new Schema({
+	name: String,
+	address: String,
+	phone: String,
+	openHours: String,
+	note: String,
+	website: String,
+	latitude: Number,
+	longitude: Number,
 });
+const BabyRoomGbg = mongoose.model("BabyRoomGbg", babyroomgbg);
+// const BabyRoomGbg = mongoose.model("BabyRoomGbg", {
+// 	name: String,
+// 	address: String,
+// 	phone: String,
+// 	openHours: String,
+// 	note: String,
+// 	website: String,
+// 	latitude: Number,
+// 	longitude: Number,
+// });
 
-
-const BabyRooms = mongoose.model('BabyRooms', {
-  name: String,
-  address: String,
-  phone: String,
-  openHours: String,
-  note: String,
-  website: String,
-  latitude: Number,
-  longitude: Number,
-  available: Boolean
+const babyrooms = new Schema({
+	name: String,
+	address: String,
+	phone: String,
+	openHours: String,
+	note: String,
+	website: String,
+	latitude: Number,
+	longitude: Number,
+	available: Boolean,
 });
+const BabyRooms = mongoose.model("BabyRooms", babyrooms);
 
-const Comment = mongoose.model('Comment', {
-  comment: {
-    type: String,
-    minLength: 5,
-    minLength: 140
-  },
-  like: {
-    type: Number,
-    default: 0
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+// const BabyRooms = mongoose.model("BabyRooms", {
+// 	name: String,
+// 	address: String,
+// 	phone: String,
+// 	openHours: String,
+// 	note: String,
+// 	website: String,
+// 	latitude: Number,
+// 	longitude: Number,
+// 	available: Boolean,
+// });
+
+const comment = new Schema({
+	comment: {
+		type: String,
+		minLength: 4,
+		maxLength: 140,
+	},
+	like: {
+		type: Number,
+		default: 0,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
 });
+const Comment = mongoose.model("Comment", comment);
+// const Comment = mongoose.model("Comment", {
+// 	comment: {
+// 		type: String,
+// 		minLength: 5,
+// 		minLength: 140,
+// 	},
+// 	like: {
+// 		type: Number,
+// 		default: 0,
+// 	},
+// 	createdAt: {
+// 		type: Date,
+// 		default: Date.now,
+// 	},
+// });
 
 if (process.env.RESET_DB) {
-  const seedDatabase = async () => {
-    try{
-      await BabyRooms.deleteMany();
-      await BabyRoomGbg.deleteMany();
-      await BabyRoomMalmo.deleteMany();
-    } catch (error) {
-      console.log(error)
-      process.exit(1);
-    }
+	const seedDatabase = async () => {
+		try {
+			await BabyRooms.deleteMany();
+			await BabyRoomGbg.deleteMany();
+			await BabyRoomMalmo.deleteMany();
+		} catch (error) {
+			console.log(error);
+			process.exit(1);
+		}
 
-    skotrum.forEach(restData => {
-      new BabyRooms(restData).save();
-    });
+		skotrum.forEach((restData) => {
+			new BabyRooms(restData).save();
+		});
 
-    gbgSkotrum.forEach(restData => {
-      new BabyRoomGbg(restData).save();
-    });
+		gbgSkotrum.forEach((restData) => {
+			new BabyRoomGbg(restData).save();
+		});
 
-    malmoSkotrum.forEach(restData => {
-      new BabyRoomMalmo(restData).save();
-    });
-  };
-  seedDatabase();
+		malmoSkotrum.forEach((restData) => {
+			new BabyRoomMalmo(restData).save();
+		});
+	};
+	seedDatabase();
 }
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -105,67 +156,60 @@ app.use(bodyParser.json());
 
 // Start defining your routes here
 
-app.get('/findBabyRooms/', async (req, res) => {
-  const babyRooms = await BabyRooms.find();
+app.get("/findBabyRooms/", async (req, res) => {
+	const babyRooms = await BabyRooms.find();
 
-  res.json(babyRooms);
-
-  console.log(babyRooms);
+	res.json(babyRooms);
 });
 
-
-
-app.get('/findBabyRooms/:id', async (req, res) => {
-  const babyRooms = await BabyRooms.findById(req.params.id);
-  if (babyRooms) {
-    res.json(babyRooms);
-  } else {
-    res.status(404).json({ error: 'babyroom not found' });
-  }
-  console.log(babyRooms);
+app.get("/findBabyRooms/:id", async (req, res) => {
+	const babyRooms = await BabyRooms.findById(req.params.id);
+	if (babyRooms) {
+		res.json(babyRooms);
+	} else {
+		res.status(404).json({ error: "babyroom not found" });
+	}
 });
 
-app.get('/gbgBabyRooms', async (req, res) => {
-  const gbgBabyRoom = await BabyRoomGbg.find();
-  console.log(gbgBabyRoom);
-  res.json(gbgBabyRoom);
+app.get("/gbgBabyRooms", async (req, res) => {
+	const gbgBabyRoom = await BabyRoomGbg.find();
+	console.log(gbgBabyRoom);
+	res.json(gbgBabyRoom);
 });
 
-app.get('/malmoBabyRooms', async (req, res) => {
-  const malmoBabyRoom = await BabyRoomMalmo.find();
-  console.log(malmoBabyRoom);
-  res.json(malmoBabyRoom);
+app.get("/malmoBabyRooms", async (req, res) => {
+	const malmoBabyRoom = await BabyRoomMalmo.find();
+	console.log(malmoBabyRoom);
+	res.json(malmoBabyRoom);
 });
 
 /// Comment endpoint
 
-app.get('/', async (req, res) => {
-  const comment = await Comment.find()
-    .sort({ createdAt: 'desc' })
-    .limit(20);
+app.get("/", async (req, res) => {
+	const comment = await Comment.find().sort({ createdAt: "desc" }).limit(20);
 
-  if (comment) {
-    res.json(comment);
-  } else {
-    res.status(404).json({ error: 'babyroom not found' });
-  }
+	if (comment) {
+		res.json(comment);
+	} else {
+		res.status(404).json({ error: "babyroom not found" });
+	}
 });
 
-app.post('/', async (req, res) => {
-  const comment = new Comment({ comment: req.body.comment });
+app.post("/", async (req, res) => {
+	const comment = new Comment({ comment: req.body.comment });
 
-  try {
-    const savedComment = await comment.save();
-    res.status(201).json(savedComment);
-  } catch (err) {
-    res.status(400).json({
-      message: 'could not add a comment to the api',
-      error: err.errors
-    });
-  }
+	try {
+		const savedComment = await comment.save();
+		res.status(201).json(savedComment);
+	} catch (err) {
+		res.status(400).json({
+			message: "could not add a comment to the api",
+			error: err.errors,
+		});
+	}
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+	console.log(`Server running on http://localhost:${port}`);
 });
